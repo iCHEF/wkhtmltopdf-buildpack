aa005d2b4378f4c0fb4d1c8583adc2e631b70260
holis <holis.hsu@ichef.com.tw>
2021-03-04 19:18:34 +0800

    fix test file

--- test/compile_test.sh
+++ test/compile_test.sh
@@ -3,5 +3,5 @@
 . ${BUILDPACK_TEST_RUNNER_HOME}/lib/test_utils.sh
 
 testGeneratePdf() {
-    assertCapturedSuccess 'wkhtmltopdf "https://www.google.com.tw/" out.pdf'
+    assertCapturedSuccess "wkhtmltopdf 'https://www.google.com.tw/' out.pdf && exit 0"
 }
