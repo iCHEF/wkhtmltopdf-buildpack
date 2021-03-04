17491607bc90c65aa3632e32438da0c31e774854
holis <holis.hsu@ichef.com.tw>
2021-03-04 20:35:19 +0800

    test

--- test/compile_test.sh
+++ test/compile_test.sh
@@ -3,7 +3,7 @@
 . ${BUILDPACK_TEST_RUNNER_HOME}/lib/test_utils.sh
 
 testCompile() {
-    capture ${BUILDPACK_HOME}/bin/compile ${BUILD_DIR}
+    capture ${BUILDPACK_HOME}/bin/compile ${BUILD_DIR} './tmp'
 
     assertEquals 0 ${rtrn}
     assertEquals "" "$(cat ${STD_ERR})"
