cd8915922f838448120a06ebdc4f24b783027a76
holis <holis.hsu@ichef.com.tw>
2021-03-04 19:20:32 +0800

    jsut test

--- test/compile_test.sh
+++ test/compile_test.sh
@@ -3,5 +3,6 @@
 . ${BUILDPACK_TEST_RUNNER_HOME}/lib/test_utils.sh
 
 testGeneratePdf() {
+    assertCapturedSuccess "exit 1"
     assertCapturedSuccess "wkhtmltopdf 'https://www.google.com.tw/' out.pdf && exit 0"
 }
