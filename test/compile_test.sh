6f211218cce18bc1f6081cde7f7c19f1a67d5a65
holis <holis.hsu@ichef.com.tw>
2021-03-04 19:45:59 +0800

    test

--- test/compile_test.sh
+++ test/compile_test.sh
@@ -3,15 +3,9 @@
 . ${BUILDPACK_TEST_RUNNER_HOME}/lib/test_utils.sh
 
 testGramma() {
-    echo 0
-
-    assertEquals 0 ${RETURN}
-    assertEquals "" "$(cat ${STD_ERR})"
+    assertEquals 0 "echo 0"
 }
 
 testGeneratePdf() {
-    wkhtmltopdf 'https://www.google.com.tw/' out.pdf
-
-    assertEquals 0 ${rtrn}
-    assertEquals "" "$(cat ${STD_ERR})"
+    assertEquals 0 "wkhtmltopdf 'https://www.google.com.tw/' out.pdf && echo 0"
 }
