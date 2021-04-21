612f66376677ecb470cab5713b2e75cdbc1d8379
holis <holis.hsu@ichef.com.tw>
2021-04-21 18:37:49 +0800

    bracnh-for-copy-wkhtmltopdf-0-12-5

---
+++ Makefile
@@ -0,0 +1,5 @@
+
+current_dir := $(shell pwd)
+
+docker-test:
+	docker run -it -v $(current_dir):/app/buildpack:ro heroku-16-buildpack-test-runner
--- test/compile_test.sh
+++ test/compile_test.sh
@@ -3,16 +3,10 @@
 . ${BUILDPACK_TEST_RUNNER_HOME}/lib/test_utils.sh
 
 testCompile() {
-    capture ${BUILDPACK_HOME}/bin/compile ${BUILD_DIR} ${BUILD_DIR}
+    ${BUILDPACK_HOME}/bin/compile ${BUILD_DIR} ${BUILD_DIR}  # this comand can see echo logs
+    # compile
 
-    assertEquals 0 ${rtrn}
-    assertEquals "" "$(cat ${STD_ERR})"
+    wkhtmltopdf http://google.com google.pdf
+
+    assertEquals "google.pdf" "$(ls -a | grep google.pdf)"
 }
-
-# testGeneratePdf() {
-#     ${BUILDPACK_HOME}/bin/compile ${BUILD_DIR}
-
-#     capture wkhtmltopdf 'https://www.google.com.tw/' out.pdf
-#     assertEquals 0 ${rtrn}
-#     assertEquals "" "$(cat ${STD_ERR})"
-# }
