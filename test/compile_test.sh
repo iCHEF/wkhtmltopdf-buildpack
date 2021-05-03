0f00a0a46969cda432b35e48f5e4fed45be1cea2
holis <holis.hsu@ichef.com.tw>
2021-05-03 13:03:52 +0800

    fix etst

--- test/compile_test.sh
+++ test/compile_test.sh
@@ -5,7 +5,7 @@
 testCompile() {
     ${BUILDPACK_HOME}/bin/compile ${BUILD_DIR} ${BUILD_DIR}  # this comand can see echo logs
 
-    ${BUILD_DIR}/bin/wkhtmltopdf http://google.com google.pdf
+    ${BUILD_DIR}/.wkhtmltopdf/bin/wkhtmltopdf http://google.com google.pdf
 
     assertEquals "google.pdf" "$(ls -a | grep google.pdf)"
 }
